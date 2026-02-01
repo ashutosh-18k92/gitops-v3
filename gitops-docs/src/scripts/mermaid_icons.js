8aa354201e8ef5b8f4d03614414c978b3b493720
ashutosh.kumar3 <ashutosh.kumar@coraltech.group>
2026-02-02 03:26:57 +0530

    mermaid icons and library link

--- gitops-docs/src/scripts/mermaid_icons.js
+++ gitops-docs/src/scripts/mermaid_icons.js
@@ -1,6 +1,11 @@
 import mermaid from "mermaid";
 import { icons } from "@iconify-json/logos";
 
+/**
+ * Icons are registered for mermaid diagrams
+ * @see https://iconify.design/docs/icon-packs/js/
+ * Pick Icons from @visit https://icones.js.org/collection/logos
+ */
 mermaid.registerIconPacks([
   {
     name: "logos", // This is the prefix you will use in your diagrams
